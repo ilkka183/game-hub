d9991e3646c178e02f22ae861d610a7e1e2fe9e7
Ilkka Salmenius <ilkka.salmenius@gmail.com>
2023-03-28 18:46:41 +0300

    Build a navbar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,13 @@
+import { Stack, HStack, VStack, Image, Text } from "@chakra-ui/react";
+import logo from "../assets/logo.webp";
+
+function NavBar() {
+  return (
+    <HStack>
+      <Image src={logo} boxSize="60px" />
+      <Text>NavBar</Text>
+    </HStack>
+  );
+}
+
+export default NavBar;
